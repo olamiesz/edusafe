9acf98aec51b24a4e9001c936ec614cc4f12f376
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2012-08-08 09:47:32 +0200

    Added template of Latocha Marcin <Marcin.Latocha@ait.ac.at>; Setup bootstrap 2.0.4

---
+++ template/php/config.php
@@ -0,0 +1,8 @@
+<?php
+$php_path = "./php/";
+
+$topbar = $php_path.'topbar.php';
+$header = $php_path.'header.php';
+$footer = $php_path.'footer.php';
+
+?>
---
+++ template/php/topbar.php
@@ -0,0 +1,22 @@
+<div class="topbar" data-dropdown="dropdown" data-scrollspy="scrollspy">
+	<div class="topbar-inner">
+		<div class="container">
+			<ul class="nav">
+				<li><a href="?link=home">Home</a></li>
+
+				<li class="dropdown">
+					<a href="?link=page1" class="dropdown-toggle">Page 1</a>
+					<ul class="dropdown-menu">
+						<li><a href="?link=page1">Some text</a></li>
+						<li class="divider"></li>
+						<li><a href="?link=page1#id1">id 1</a></li>
+						<li><a href="?link=page1#id2">id 2</a></li>
+					</ul>
+				</li>
+
+				<li><a href="?link=page2">Page 2</a></li>
+				
+			</ul>
+		</div>
+	</div><!-- /topbar-inner -->
+</div><!-- /topbar -->
