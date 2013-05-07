c1847e5f736f6e42976e5ccc636da7476976f0be
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2013-05-07 11:50:39 +0200

    Added fellows

---
+++ jobs/Fellows.php
@@ -0,0 +1,103 @@
+
+	<div class="hero-unit">
+		<h1>Fellows</h1>
+	</div>
+
+	<div class="row-fluid">
+<dl>
+<dt>ESR1:</dt>
+<dt>Start date:</dt> 
+<dt>Supervisor:</dt><dd>Giulio Aielli</dd>
+<dt>Host Organization:</dt><dd>University of Roma "Tor Vergata"</dd>
+<dt>Research Project:</dt> 
+</dl>
+
+<dl>
+<dt>ER1:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Giulio Aielli</dd> 
+<dt>Host Organization:</dt><dd>University of Roma "Tor Vergata"</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR2:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Vincent Lepetit</dd>
+<dt>Host Organization:</dt><dd>Ecole Polytechnique Fédérale de Lausanne (EPFL)</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ER2:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Vincent Lepetit</dd>
+<dt>Host Organization:</dt><dd>Ecole Polytechnique Fédérale de Lausanne (EPFL)</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR3:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt>
+<dt>Host Organization:</dt><dd>NOVOCAPTIS</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR4:</dt><dd>Lakshmi Prabha Nattamai Sekar</dd>
+<dt>Start date:</dt><dd>1.4.2013</dd>
+<dt>Supervisor:</dt><dd>Olga Beltramello</dd>
+<dt>Host Organization:</dt><dd>CERN</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR5:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Evangelos Gazis</dd>
+<dt>Host Organization:</dt><dd>National Technical University of Athens (NTUA)</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR6:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Petros Soukoulias</dd>
+<dt>Host Organization:</dt><dd>PRISMA Electronics SA</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR7:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Nabil Menaa</dd>
+<dt>Host Organization:</dt><dd>Canberra France</dd>
+<dt>Research Project:</dt>
+</dl>
+
+<dl>
+<dt>ESR8:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Nabil Menaa</dd>
+<dt>Host Organization:</dt><dd>Canberra France</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR9:</dt><dd>Yuta Itoh</dd>
+<dt>Start date:</dt><dd>1.4.2013</dd>
+<dt>Supervisor:</dt><dd>Gudrun Klinker</dd>
+<dt>Host Organization:</dt><dd>Technische Universität München (TUM)</dd>
+<dt>Research Project:</dt>
+</dl> 
+
+<dl>
+<dt>ESR10:</dt>
+<dt>Start date:</dt>
+<dt>Supervisor:</dt><dd>Theodoros Apostolopoulos
+<dt>Host Organization:</dt><dd>Athens University of Economics & Business (AUEB)
+<dt>Research Project:</dt>
+</dl>
+ 
+	</div>
